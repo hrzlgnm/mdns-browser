d574b057b4c3404e61992b34af33d6cef97257ff
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-04-30 22:27:06 +0200

    chore(cleanup): remove some debug log leftovers
    rename some vars to clarify their meaning

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use app::*;
 use leptos::*;
 
 fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
+    _ = console_log::init_with_level(log::Level::Info);
     console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! { <App/> }
