398c5af9cd6d8a456a4e06bde0389083419c78f6
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-05-11 03:18:20 +0200

    feat: remove resolve workflow (#135)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use if_addrs::{get_if_addrs, IfAddr, Interface};
 use log::LevelFilter;
 use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
 use serde::Serialize;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashMap,
     net::IpAddr,
     sync::{Arc, Mutex},
     time::{Duration, SystemTime},
@@ -18,7 +17,6 @@ type SharedServiceDaemon = Arc<Mutex<ServiceDaemon>>;
 
 struct MdnsState {
     daemon: SharedServiceDaemon,
-    resolved_address_filters: Arc<Mutex<Vec<Interface>>>,
     running_browsers: Arc<Mutex<Vec<String>>>,
 }
 
@@ -26,7 +24,6 @@ impl MdnsState {
     fn new() -> Self {
         Self {
             daemon: get_shared_daemon(),
-            resolved_address_filters: Arc::new(Mutex::new(get_all_interfaces_except_loopback())),
             running_browsers: Arc::new(Mutex::new(Vec::new())),
         }
     }
@@ -212,169 +209,10 @@ fn browse(service_type: String, window: Window, state: State<MdnsState>) {
     }
 }
 
-#[tauri::command]
-fn resolve_service(service_type: String, state: State<MdnsState>) -> Vec<ResolvedService> {
-    if service_type.is_empty() {
-        return vec![];
-    }
-    log::debug!("Resolving {}", service_type);
-    let mdns = state.daemon.lock().unwrap();
-    let mut service_type = service_type;
-    if !service_type.ends_with(".local.") {
-        service_type.push_str(".local.");
-    }
-    let receiver = mdns
-        .browse(service_type.as_str())
-        .expect("Failed to browse");
-    let mut result = HashMap::new();
-    let mut searches_started = 0;
-    while let Ok(event) = receiver.recv() {
-        match event {
-            ServiceEvent::ServiceResolved(info) => {
-                let mut key = info.get_fullname().to_string();
-                key.push_str(info.get_hostname());
-                result.insert(key, ResolvedService::from(&info));
-            }
-            ServiceEvent::SearchStarted(_) => {
-                if searches_started > 3 || !result.is_empty() {
-                    mdns.stop_browse(service_type.as_str())
-                        .expect("To stop browsing");
-                }
-                searches_started += 1;
-            }
-            ServiceEvent::SearchStopped(_) => {
-                break;
-            }
-            _ => {}
-        }
-    }
-    let mut filtered = filter_resolved_service_by_interfaces_addresses(
-        result.values().cloned().collect(),
-        state.resolved_address_filters.lock().unwrap().clone(),
-    );
-    filtered.sort_by(|a, b| a.instance_name.cmp(&b.instance_name));
-
-    filtered
-}
-
-fn valid_ip_on_interface(addr: &IpAddr, interface: &Interface) -> bool {
-    match (addr, &interface.addr) {
-        (IpAddr::V4(addr), IfAddr::V4(interface_address)) => {
-            let netmask = u32::from(interface_address.netmask);
-            let interface_net = u32::from(interface_address.ip) & netmask;
-            let addr_net = u32::from(*addr) & netmask;
-            addr_net == interface_net
-        }
-        (IpAddr::V6(addr), IfAddr::V6(interface_address)) => {
-            let netmask = u128::from(interface_address.netmask);
-            let interface_net = u128::from(interface_address.ip) & netmask;
-            let addr_net = u128::from(*addr) & netmask;
-            addr_net == interface_net
-        }
-        _ => false,
-    }
-}
-
-fn get_addresses_on_interface(addr: &[IpAddr], interface: &Interface) -> Vec<IpAddr> {
-    addr.iter()
-        .filter(|a| valid_ip_on_interface(a, interface))
-        .copied()
-        .collect()
-}
-
-fn filter_resolved_service_by_interfaces_addresses(
-    resolved_services: Vec<ResolvedService>,
-    interfaces: Vec<Interface>,
-) -> Vec<ResolvedService> {
-    if interfaces.is_empty() {
-        return resolved_services;
-    }
-    let mut result = Vec::<ResolvedService>::new();
-    for resolved_service in resolved_services.iter() {
-        let mut unique_addresses = HashSet::<IpAddr>::new();
-        for interface in interfaces.iter() {
-            unique_addresses.extend(get_addresses_on_interface(
-                &resolved_service.addresses,
-                interface,
-            ));
-        }
-        let mut addresses = unique_addresses.into_iter().collect::<Vec<_>>();
-        if !addresses.is_empty() {
-            addresses.sort();
-            result.push(ResolvedService {
-                instance_name: resolved_service.instance_name.clone(),
-                hostname: resolved_service.hostname.clone(),
-                port: resolved_service.port,
-                addresses,
-                subtype: resolved_service.subtype.clone(),
-                txt: resolved_service.txt.clone(),
-                updated_at_ms: resolved_service.updated_at_ms,
-            });
-        }
-    }
-    result
-}
-
-fn get_all_interfaces_except_loopback() -> Vec<Interface> {
-    let interface_addresses = get_if_addrs().unwrap();
-
-    interface_addresses
-        .into_iter()
-        .filter(|itf| !itf.is_loopback())
-        .collect()
-}
-
-fn get_all_interface_names_except_loopback() -> Vec<String> {
-    let interface_addresses = get_all_interfaces_except_loopback();
-
-    interface_addresses
-        .into_iter()
-        .map(|interface| interface.name)
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect()
-}
-
-#[tauri::command]
-fn list_filter_interfaces() -> Vec<String> {
-    let mut interface_names = get_all_interface_names_except_loopback();
-    interface_names.sort();
-
-    interface_names
-}
-
-#[tauri::command]
-fn get_filter_interface(state: State<MdnsState>) -> HashSet<String> {
-    if let Ok(filters) = state.resolved_address_filters.lock() {
-        return filters
-            .clone()
-            .into_iter()
-            .map(|itf| itf.name.clone())
-            .collect::<HashSet<_>>();
-    }
-
-    HashSet::new()
-}
-
-#[tauri::command]
-fn set_filter_interfaces(interfaces: HashSet<String>, state: State<MdnsState>) {
-    let interface_names = get_all_interface_names_except_loopback();
-
-    let enabled_interface_names = interface_names
-        .into_iter()
-        .filter(|name| interfaces.contains(name))
-        .collect::<Vec<_>>();
-    let enabled_interfaces = get_all_interfaces_except_loopback()
-        .into_iter()
-        .filter(|interface| enabled_interface_names.contains(&interface.name))
-        .collect::<Vec<_>>();
-    *state.resolved_address_filters.lock().unwrap() = enabled_interfaces;
-}
-
 const METRIC_SEND_INTERVAL: Duration = Duration::from_millis(200);
 
 #[tauri::command]
-fn metrics_sender(window: Window, state: State<MdnsState>) {
+fn send_metrics(window: Window, state: State<MdnsState>) {
     if let Ok(mdns) = state.daemon.lock() {
         let mdns_for_thread = mdns.clone();
         std::thread::spawn(move || loop {
@@ -433,11 +271,7 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             browse,
             browse_types,
-            get_filter_interface,
-            list_filter_interfaces,
-            metrics_sender,
-            resolve_service,
-            set_filter_interfaces,
+            send_metrics,
             stop_browse
         ])
         .run(tauri::generate_context!())
