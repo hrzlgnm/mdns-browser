d80a59ccf9535dc544fde2010eff08e5f99e01bb
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-11 20:34:18 +0100

    fix: workaround not being able to set autocapitalize attribute to inputs

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Workaround for thaw inputs wrapped by components `AutoComplete` or `Input`
+ * having no possibility to set autocapitalize="none" which
+ * prevents mobile inputs to auto-capitalize the first letter,
+ * which is what we want
+ */
+window.addEventListener("TrunkApplicationStarted", function () {
+    const inputs = document.getElementsByClassName("thaw-input__input");
+    for (const input of inputs) {
+        input.setAttribute("autocapitalize", "none");
+    }
+});
