8da1bafaf69f78e05e04f3dc65e11c990aa474b3
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-12 18:59:20 +0100

    feat: disable spellchecks and autocorrection along with autocapitalization
    Closes #806

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /**
  * Workaround for thaw inputs wrapped by components `AutoComplete` or `Input`
- * having no possibility to set autocapitalize="none" which
- * prevents mobile inputs to auto-capitalize the first letter,
- * which is what we want
+ * providing no possibility to set attributes.
+ * We want no spell checking no autocorrection or capitalization in our inputs 
+ * mostly dealing with lower case characters.
  */
 window.addEventListener("TrunkApplicationStarted", function () {
     const inputs = document.querySelectorAll("input.thaw-input__input");
     for (const input of inputs) {
         input.setAttribute("autocapitalize", "none");
+        input.setAttribute("autocorrect", "off");
+        input.setAttribute("spellcheck", "false");
     }
 });
