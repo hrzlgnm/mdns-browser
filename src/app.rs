10c2e412c2f363f1bf9c17b16e224326074addf8
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-17 20:00:48 +0100

    Deref

--- src/app.rs
+++ src/app.rs
@@ -35,7 +35,7 @@ macro_rules! log_fn {
 fn get_class(is_desktop: &ReadSignal<bool>, base_class: &str) -> Signal<String> {
     let base_class = base_class.to_string();
     Signal::derive({
-        let is_desktop = is_desktop.clone();
+        let is_desktop = *is_desktop;
         move || {
             let prefix = if is_desktop.get() {
                 "desktop-"
