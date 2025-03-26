9973e7fe1876a94698b68e8a7f47e9a0f6a2276d
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-27 00:12:13 +0100

    a

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * mostly dealing with lower case characters.
  */
 window.addEventListener("TrunkApplicationStarted", function () {
-    const browseButton = document.querySelector("button.thaw-button--primary[contains(text(), 'Browse')]");
+    const browseButton = document.querySelector("button.thaw-button--primary");
     const inputs = document.querySelectorAll("input.thaw-input__input");
     for (const input of inputs) {
         input.setAttribute("autocapitalize", "none");
