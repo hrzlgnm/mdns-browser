12c3f7d38c4f8d388168a4a2ddfdcd652cf93ddc
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-27 00:34:42 +0100

    feat: Pressing enter in quick filter starts browsing (#866)

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 /**
- * Workaround for thaw inputs wrapped by components `AutoComplete` or `Input`
- * providing no possibility to set attributes.
+ * Workarounds for thaw inputs wrapped by components `AutoComplete` or `Input`
+ * providing no possibility to set attributes or to add onkeydown event listeners.
  * We want no spell checking no autocorrection or capitalization in our inputs 
  * mostly dealing with lower case characters.
  */
 window.addEventListener("TrunkApplicationStarted", function () {
+    const browseButton = document.querySelector("button.thaw-button--primary");
     const inputs = document.querySelectorAll("input.thaw-input__input");
     for (const input of inputs) {
         input.setAttribute("autocapitalize", "none");
         input.setAttribute("autocorrect", "off");
         input.setAttribute("spellcheck", "false");
+        if (input.getAttribute("placeholder")?.includes("Quick")) {
+            input.addEventListener("keydown", function (event) {
+                if (event.key === "Enter") {
+                    if (browseButton) browseButton.click();
+                }
+            });
+        }
     }
 });
