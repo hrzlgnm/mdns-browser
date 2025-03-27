461610bbe25eec5e79661b688cd8cfcb706a7094
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-27 20:48:44 +0100

    refactor: Move inline stlyes to CSS

--- src/app/browse.rs
+++ src/app/browse.rs
@@ -455,8 +455,8 @@ fn ResolvedServiceItem(resolved_service: ResolvedService) -> impl IntoView {
                                     </Button>
                                     <Dialog open=show_details>
                                         <DialogSurface>
-                                            <DialogBody attr:style="display: flex; max-width: 90vw;">
-                                                <Scrollbar style="max-height: 90vh;">
+                                            <DialogBody class="details-dialog">
+                                                <Scrollbar class="details-dialog-scrollarea">
                                                     <Flex vertical=true>
                                                         <DialogTitle>{details_title}</DialogTitle>
                                                         <ValuesTable values=subtype title="subtype" />
