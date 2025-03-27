496c33cb980a50f37b25974f591715e65eb83ccd
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-03-27 21:14:21 +0100

    refactor: Make debug logging a noop in log_fn macro in release builds (#871)

--- src/app/macros.rs
+++ src/app/macros.rs
@@ -1,9 +1,14 @@
 #[macro_export]
 macro_rules! log_fn {
     ($name:expr, $body:block) => {{
+        #[cfg(debug_assertions)]
         log::debug!("-> {}", $name);
+
         let result = { $body };
+
+        #[cfg(debug_assertions)]
         log::debug!("<- {}", $name);
+
         result
     }};
 }
