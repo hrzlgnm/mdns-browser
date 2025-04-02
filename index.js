8435c463f397a000df90be755818c0a2005c6cc3
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-04-02 21:01:43 +0200

    cleanup

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ window.addEventListener("TrunkApplicationStarted", function () {
         if (input.getAttribute("placeholder")?.includes("Quick")) {
             input.addEventListener("keydown", function (event) {
                 if (event.key === "Enter") {
-                    if (browseButton) browseButton.click();
+                    if (browseButton) {
+                        browseButton.click();
+                    }
                 }
             });
         }
         if (input.getAttribute("placeholder")?.includes("Service")) {
             const autocomplete = document.querySelector("div.thaw-auto-complete");
             input.addEventListener("keydown", function (event) {
-                const inputIsValid = autocomplete.getAttribute("class")?.includes("service-type-valid");
+                const inputIsValid = autocomplete?.getAttribute("class")?.includes("service-type-valid");
                 if (event.key === "Enter" && inputIsValid) {
                     if (browseButton) {
+                        browseButton.click();
+                    }
                 }
             });
         }
