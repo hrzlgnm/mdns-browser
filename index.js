50ecfc65e13feb9085031fd12355cc54d82c9f59
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-06-09 16:16:13 +0200

    feat: Disable drag-and-drop interactions to prevent uninended navigation (#1180)

--- index.js
+++ index.js
@@ -3,8 +3,17 @@
  * providing no possibility to set attributes or to add onkeydown event listeners.
  * We want no spell checking no autocorrection or capitalization in our inputs
  * mostly dealing with lower case characters.
+ * Also turn off drop events globally by just ignoring swallowing those
  */
 window.addEventListener("TrunkApplicationStarted", function () {
+    document.addEventListener("dragover", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+    });
+    document.addEventListener("drop", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+    });
     const browseButton = document.querySelector("button.thaw-button--primary");
     const inputs = document.querySelectorAll("input.thaw-input__input");
     const autocomplete = document.querySelector("div.thaw-auto-complete");
