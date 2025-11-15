276e96ac3c967b0855f8a3ff4fcdca10a1d6004c
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-11-15 16:37:05 +0100

    strip explicititly and generate no debug pacakge
    The debug package is useless, as it only contains the symbols and no
    debuginfo.

--- aur-template/generate-mdns-browser-bin.sh
+++ aur-template/generate-mdns-browser-bin.sh
@@ -17,11 +17,14 @@ arch=('x86_64')
 url="https://github.com/hrzlgnm/mdns-browser"
 license=('MIT')
 depends=('cairo' 'desktop-file-utils' 'gdk-pixbuf2' 'glib2' 'gtk3' 'hicolor-icon-theme' 'libsoup' 'pango' 'webkit2gtk-4.1')
-options=('!emptydirs')
+options=('!strip !emptydirs')
 conflicts=('mdns-browser')
 source_x86_64=("https://github.com/hrzlgnm/mdns-browser/releases/download/mdns-browser-v\$pkgver/mdns-browser_\${pkgver}_amd64.deb")
 sha256sums_x86_64=('$sha256sum')
 package() {
     tar -xz -f data.tar.gz -C "\${pkgdir}"
+    # Explicitly strip only the binary; !strip in options prevents makepkg's auto-strip phase which also generates 
+    # a debug package, which is useless here, as the debug information only contains symbols and no debug info
+    strip "\${pkgdir}/usr/bin/mdns-browser"
 }
 EOF
