66c9b3b81ca843a11e2aa829481df440b713bb91
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-11-30 12:50:27 +0100

    Revert "fix: Add workaround --no-sign not working as expected (#1687)"
    This reverts commit 0c5f3b16b4dad2c39b5949dcedecdd14c10ffd40.

--- aur-template/generate-mdns-browser.sh
+++ aur-template/generate-mdns-browser.sh
@@ -2,6 +2,7 @@
 # Copyright 2024-2025 hrzlgnm
 # SPDX-License-Identifier: MIT-0
 
+
 version=$1
 sha256sum=$2
 
@@ -40,9 +41,7 @@ build() {
     export CFLAGS="\${CFLAGS//-flto=auto//}"
     # build the normal binary without bundling first
     cargo --locked --frozen auditable tauri build --no-bundle
-    # The --no-sign option does not seem to work, therefore we need to swallow any errors here
-    # See https://github.com/tauri-apps/tauri/issues/14581 for more information
-    cargo --locked --frozen auditable tauri build -b deb --no-sign || true
+    cargo --locked --frozen auditable tauri build -b deb --no-sign
 }
 check() {
     cd "\$srcdir/\$_builddir" || exit 1
