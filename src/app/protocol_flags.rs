f1ee71d029e24bac681eec1b9fafab114fdcfefc
hrzlgnm <hrzlgnm@users.noreply.github.com>
2026-02-21 10:47:42 +0100

    chore: remove debug logging statements

--- src/app/protocol_flags.rs
+++ src/app/protocol_flags.rs
@@ -24,7 +24,6 @@ use super::browse::browse_types;
 /// ```
 async fn get_protocol_flags(store: Store<ProtocolFlags>) {
     let flags = invoke::<ProtocolFlags>("get_protocol_flags", &()).await;
-    log::debug!("get_protocol_flags: {flags:?}");
     store.set(flags);
 }
 
